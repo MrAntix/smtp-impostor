c56f3e8705da65f280a017ace2d64c35e9966717
Anthony Johnston <aj@antix.co.uk>
2019-10-11 23:49:35 +0100

    feat(webSocket): reconnect backoff

--- SMTP.Impostor.Worker/src/impostor-hub/component.tsx
+++ SMTP.Impostor.Worker/src/impostor-hub/component.tsx
@@ -4,6 +4,9 @@ import {
   HubState, IHubMessage
 } from './model';
 
+const RECONNECT_INIT = 200;
+const RECONNECT_MAX = 15000;
+
 @Component({
   tag: 'impostor-hub',
   styleUrl: 'component.css',
@@ -12,6 +15,7 @@ import {
 export class ImpostorHubComponent {
   logger = globalThis.getLogger('ImpostorHubComponent');
   socket: IHubSocket;
+  reconnectIn = 200;
 
   @Prop() socketProvider: IHubSocketProvider = hubSocketProvider;
   @Prop() url: string = `wss://${location.host}/hub`;
@@ -22,7 +26,7 @@ export class ImpostorHubComponent {
   ) {
     if (url) this.url = url;
 
-    this.logger.info('connect', { url: this.url });
+    this.logger.info('connecting', { url: this.url });
 
     try {
       if (this.socket) this.disconnectAsync();
@@ -32,6 +36,7 @@ export class ImpostorHubComponent {
       this.socket.onopen = (e: any) => {
         this.logger.debug('socket.onopen', { e });
 
+        this.reconnectIn = RECONNECT_INIT;
         this.setState(HubState.connected);
       };
 
@@ -39,6 +44,7 @@ export class ImpostorHubComponent {
         this.logger.info('socket.onclose', { e });
 
         this.disconnectAsync();
+        this.retryConnect();
       };
 
       this.socket.onerror = (e: any) => {
@@ -61,11 +67,22 @@ export class ImpostorHubComponent {
     } catch (err) {
       this.logger.error('connect', err);
       this.setState(HubState.error);
+      this.retryConnect();
     }
   }
 
+  retryConnect() {
+    this.logger.info('retryConnect', this.reconnectIn);
+
+    setTimeout(() => this.connectAsync(), this.reconnectIn);
+
+    this.reconnectIn *= 2
+    if (this.reconnectIn > RECONNECT_MAX)
+      this.reconnectIn = RECONNECT_MAX;
+  }
+
   @Method() async sendAsync(message: IHubMessage) {
-    if(!this.socket) throw new Error('cannot send, not connected');
+    if (!this.socket) throw new Error('cannot send, not connected');
 
     this.socket.send(
       JSON.stringify({
@@ -77,7 +94,7 @@ export class ImpostorHubComponent {
 
   @Method() async disconnectAsync() {
     this.logger.debug('disconnectAsync');
-    if (this.socket.readyState === WebSocket.OPEN) {
+    if (this.socket.readyState < WebSocket.CLOSING) {
       this.logger.debug('disconnectAsync socket.close');
       this.socket.close()
     }
