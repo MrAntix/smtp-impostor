e629269a4cdd6bb17b90ac5231e2c66e208bb900
Anthony Johnston <aj@antix.co.uk>
2019-10-25 17:53:35 +0100

    updated test sender

--- SMTP.Impostor.Test.Sender/MainWindow.xaml.cs
+++ SMTP.Impostor.Test.Sender/MainWindow.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.IO;
 using System.Net.Mail;
-using System.Net.Mime;
 using System.Windows;
 
 namespace SMTP.Impostor.Test.Sender
@@ -19,39 +17,52 @@ namespace SMTP.Impostor.Test.Sender
         string HostName { get; set; } = "localhost";
         int Port { get; set; } = 25;
 
-        private async void button_Click(object sender, RoutedEventArgs e)
+        private async void SendButton_Click(object sender, RoutedEventArgs e)
         {
-            using var client = new SmtpClient(HostName, Port);
-            using var mailMessage = new MailMessage
+            SendButton.IsEnabled = false;
+            try
             {
-                From = new MailAddress("a@example.com"),
-                Subject = "SUBJECT"
-            };
-            mailMessage.To.Add("b@example.com");
-            mailMessage.CC.Add("c@example.com");
-            mailMessage.CC.Add("d@example.com");
+                Status.Content = "sending, please wait...";
+                using var client = new SmtpClient(HostName, Port);
+                using var mailMessage = new MailMessage
+                {
+                    From = new MailAddress(FromControl.Text),
+                    Subject = SubjectControl.Text
+                };
+                mailMessage.To.Add(ToControl.Text);
+                //mailMessage.CC.Add("c@example.com");
+                //mailMessage.CC.Add("d@example.com");
 
-            //mailMessage.Body = TestResources.HTML_EMAIL;
-            //mailMessage.IsBodyHtml = true;
+                //mailMessage.Body = TestResources.HTML_EMAIL;
+                
+                mailMessage.Body = BodyControl.Text;
+                mailMessage.IsBodyHtml = IsHTMLControl.IsChecked.GetValueOrDefault();
 
-            mailMessage.Body = "SOME CONTENT\nSOME CONTENT\nSOME CONTENT\nSOME CONTENT\n";
+                //var alternate = AlternateView.CreateAlternateViewFromString(
+                //    $"<p>{mailMessage.Body}</p>",
+                //    new ContentType("text/html"));
+                //mailMessage.AlternateViews.Add(alternate);
 
-            var alternate = AlternateView.CreateAlternateViewFromString(
-                $"<p>{mailMessage.Body}</p>",
-                new ContentType("text/html"));
-            mailMessage.AlternateViews.Add(alternate);
+                //var buffer = new byte[10 * 1000 * 1000];
+                //for (var i = 0; i < buffer.Length; i++)
+                //    buffer[i] = 0;
 
-            var buffer = new byte[10 * 1000 * 1000];
-            for (var i = 0; i < buffer.Length; i++)
-                buffer[i] = 0;
+                //using var stream = new MemoryStream(buffer);
+                //mailMessage.Attachments.Add(new Attachment(
+                //    stream, new ContentType("application/app")
+                //    ));
 
-            using var stream = new MemoryStream(buffer);
-            mailMessage.Attachments.Add(new Attachment(
-                stream, new ContentType("application/app")
-                ));
-
-            for (var i = 0; i < 10; i++)
+                //for (var i = 0; i < 10; i++)
                 await client.SendMailAsync(mailMessage);
+
+                Status.Content = "message sent";
+            }
+            catch (Exception ex)
+            {
+                Status.Content = ex.Message;                
+            }
+
+            SendButton.IsEnabled = true;
 
             GC.Collect();
         }
